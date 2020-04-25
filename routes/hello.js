d6e38b631e1bc44a5e2f4e0309c79acf82afb178
Shusuke Nakamura <shuchan.galaxy@gmail.com>
2020-04-25 20:14:32 +0900

    クエリーパラメータの実装

--- routes/hello.js
+++ routes/hello.js
@@ -1,10 +1,12 @@
 var express = require('express');
 var router = express.Router();
 
-router.get('/', (req, res, next) => { 
+router.get('/', (req, res, next) => {
+  var name = req.query.name;
+  var mail = req.query.mail;
   var data = {
     title: 'Hello!',
-    content: 'これは、サンプルのコンテンツです。<br> this is sample content.'
+    content: 'あなたの名前は、' + name + '。<br>' + 'メールアドレスは、' + mail + 'です。'
   };
   res.render('hello', data);
 });
