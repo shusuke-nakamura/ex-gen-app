2f1e7d3df9ee6e3962f1dc20f5f086b7c033f95d
Shusuke Nakamura <shuchan.galaxy@gmail.com>
2020-04-25 20:27:16 +0900

    formの送信(POST)のサンプル

--- routes/hello.js
+++ routes/hello.js
@@ -2,11 +2,21 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', (req, res, next) => {
-  var name = req.query.name;
-  var mail = req.query.mail;
+  // var name = req.query.name;
+  // var mail = req.query.mail;
   var data = {
     title: 'Hello!',
-    content: 'あなたの名前は、' + name + '。<br>' + 'メールアドレスは、' + mail + 'です。'
+    // content: 'あなたの名前は、' + name + '。<br>' + 'メールアドレスは、' + mail + 'です。'
+    content: '※何か書いて送信してください。'
+  };
+  res.render('hello', data);
+});
+
+router.post('/post', (req, res, next) => {
+  var msg = req.body['message'];
+  var data = {
+    title: 'Hello!',
+    content: 'あなたは、「' + msg + '」と送信しました。'
   };
   res.render('hello', data);
 });
