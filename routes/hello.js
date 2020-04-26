88786dbb337d345daeddb7b237517192ef6718eb
Shusuke Nakamura <shuchan.galaxy@gmail.com>
2020-04-26 16:29:37 +0900

    P.256まで学習完了

--- routes/hello.js
+++ routes/hello.js
@@ -4,19 +4,24 @@ var router = express.Router();
 router.get('/', (req, res, next) => {
   // var name = req.query.name;
   // var mail = req.query.mail;
+  var msg = "※何か書いて送信して下さい。";
+  if (req.session.message != undefined) {
+    msg = "Last Message: " + req.session.message;
+  }
   var data = {
     title: 'Hello!',
     // content: 'あなたの名前は、' + name + '。<br>' + 'メールアドレスは、' + mail + 'です。'
-    content: '※何か書いて送信してください。'
+    content: msg
   };
   res.render('hello', data);
 });
 
 router.post('/post', (req, res, next) => {
   var msg = req.body['message'];
+  req.session.message = msg;
   var data = {
     title: 'Hello!',
-    content: 'あなたは、「' + msg + '」と送信しました。'
+    content: "Last Message: " + req.session.message
   };
   res.render('hello', data);
 });
