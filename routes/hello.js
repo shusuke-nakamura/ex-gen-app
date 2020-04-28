1d98ebf6832384773a6b4c714eecb15cc2914abe
Shusuke Nakamura <shuchan.galaxy@gmail.com>
2020-04-28 19:45:04 +0900

    P.271 RSSの取得まで学習完了

--- routes/hello.js
+++ routes/hello.js
@@ -1,29 +1,34 @@
 var express = require('express');
 var router = express.Router();
 
+var http = require('https');
+var parseString = require('xml2js').parseString;
+
 router.get('/', (req, res, next) => {
-  // var name = req.query.name;
-  // var mail = req.query.mail;
-  var msg = "※何か書いて送信して下さい。";
-  if (req.session.message != undefined) {
-    msg = "Last Message: " + req.session.message;
-  }
-  var data = {
-    title: 'Hello!',
-    // content: 'あなたの名前は、' + name + '。<br>' + 'メールアドレスは、' + mail + 'です。'
-    content: msg
+
+  var opt = {
+    host: 'news.google.com',
+    port: 443,
+    path: '/rss?ie=UTF-8&oe=UTF-8&hl=en-US&gl=US&ceid=US:en'
   };
-  res.render('hello', data);
+
+  http.get(opt, (res2) => {
+    var body = '';
+    res2.on('data', (data) => {
+      body += data;
+    });
+
+    res2.on('end', () => {
+      parseString(body.trim(), (err, result) => {
+        var data = {
+          title: 'Hello!',
+          content: result.rss.channel[0].item
+        };
+        res.render('hello', data);
+      });
+    });
+  });
 });
 
-router.post('/post', (req, res, next) => {
-  var msg = req.body['message'];
-  req.session.message = msg;
-  var data = {
-    title: 'Hello!',
-    content: "Last Message: " + req.session.message
-  };
-  res.render('hello', data);
-});
 
 module.exports = router;
